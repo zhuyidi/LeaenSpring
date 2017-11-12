053ec9f5f12916091e1bd835bcf5edc07e2628e9
zhuyidi <1311243087@qq.com>
2017-11-12 16:02:57 +0800

    update

---
+++ Spring_10/src/main/java/Spittr/Model/Spitter.java
@@ -0,0 +1,100 @@
+package Spittr.Model;
+
+import com.sun.istack.internal.NotNull;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import javax.validation.constraints.Size;
+
+
+/**
+ * Created by dela on 11/9/17.
+ */
+public class Spitter {
+    private Long id;
+
+    @NotNull
+    @Size(min=5, max=16)
+    private String username;
+
+    @NotNull
+    @Size(min=5, max=25)
+    private String password;
+
+    @NotNull
+    @Size(min=2, max=30)
+    private String firstName;
+
+    @NotNull
+    @Size(min=2, max=30)
+    private String lastName;
+
+    public Spitter(String firstName, String lastName, String username, String password){
+        this(null, firstName, lastName, username, password);
+    }
+
+    public Spitter(Long id, String firstName, String lastName, String username, String password){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+
+
+
+    public Spitter(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        return EqualsBuilder.reflectionEquals(this, that, "firstName", "lastName", "username", "password");
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this, "firstName", "lastName", "username", "password");
+    }
+
+}
